f5e20a28a70cb450a6f8a9d94bffa02bf43fa18a
lagalapizza_1011 <52937131+SH0-ahacker@users.noreply.github.com>
2021-01-03 21:00:09 -0300

    Create .js
    V1.0.0

---
+++ .js
@@ -0,0 +1,20 @@
+//TOFUBLOG
+// BLOGGER/BLOGSPOT
+//  SPAMWEBSITE V1.0.0
+//   GROW YOUR WEBSITE IM MINUTES!
+//<3
+function tofublogStart(){
+console.log("running")
+	let bloggerID = prompt("Blogger/blogspot page URL:\n(put https)")
+	console.log("URL asked")
+let run = prompt("You really wants that Tofublog spam the blogger website?\nyes or not???")
+console.log("Confirmation asked")
+console.log("start running")
+if (run == "yes") {
+	setInterval(function(){
+	window.open(bloggerID)
+	console.log("opened")}, 1000);
+	} //1000= 1sec, 2000= 2sec.^    
+}       //                   etc.  
+tofublogStart()
+//^ checker from the dev
